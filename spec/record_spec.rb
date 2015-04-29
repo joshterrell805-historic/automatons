ead4c696e4a9310876fc23f6ca7d18d031b9ca8d
Andrew Gilbert <andrewg800@gmail.com>
2015-04-29 04:46:26 -0700

    More work on spec for Record
    This Record class looks like it'll be more a SourceRecord class,
    eventually

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -1,13 +1,41 @@
 require 'spec_helper'
+require 'example'
 
 module MDM
    describe Record do
       before :each do
-         @record = Record.new 
+         @record = Example.get_standard_record
       end
+
+      describe "#clean" do
+         it "converts ###-###-#### phone numbers to (###) ###-####" do
+            @record.phone = "555-555-5555"
+            @record.clean
+            expect(@record.phone).to eq("(555) 555-5555")
+         end
+      end
+
+      describe "#initialize" do
+         it "parses record" do
+            record = Record.new(Example::STANDARD_RECORD)
+            expect(record.id).to eq('24614')
+            expect(record.type).to eq('Individual')
+            expect(record.name).to eq('Christina L Grant')
+            expect(record.gender).to eq('F')
+            expect(record.dob).to eq('01-01-2000')
+            expect(record.sole_proprietor).to eq('N')
+            expect(record.phone).to eq('(555) 555-5555')
+            expect(record.primary_specialty).to eq('390200000X')
+            expect(record.secondary_specialty).to eq('390200000Y')
+         end
+      end
+
       describe "#==" do
-         it "returns false if other record is nil" do
-            expect(
+         it "does normal equality" do
+            expect(@record == nil).to be false
+            expect(@record == Class).to be false
+            expect(@record == Record.new('{}')).to be false
+            expect(@record == Example.get_standard_record).to be true
          end
       end
    end
