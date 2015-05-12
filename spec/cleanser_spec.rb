dfcb5fb5cdeeb7b91e77b9470f58628ed1cce7a9
Andrew Gilbert <andrewg800@gmail.com>
2015-05-12 01:22:25 -0700

    Add spec for Cleanser#run_rule
    Will be used to easily run a rule by name for testing

--- spec/cleanser_spec.rb
+++ spec/cleanser_spec.rb
@@ -5,6 +5,29 @@ describe Cleanser do
       @cleanser = Cleanser.new
    end
 
+   describe "#run_rule" do
+      before :each do
+         @cleanser.add do
+            rule :thing, /^abc/, "first" do
+               "changed"
+            end
+
+            rule :thing, //, "second" do
+               "wrong"
+            end
+         end
+      end
+
+      it "runs a named rule" do
+         expect(@cleanser.run_rule "first", {:thing => "abc"}).to eq({:thing => "changed"})
+      end
+
+      it "respects the rule matcher" do
+         expect(@cleanser.run_rule "first", {:thing => "gef"}).to eq({:thing => "gef"})
+         expect(@cleanser.run_rule "first", {:thing => "abcdef"}).to eq({:thing => "changed"})
+      end
+   end
+
    describe "#dump_rules" do
       it "returns an empty list if there are no rules" do
          expect(@cleanser.dump_rules).to eq([])
