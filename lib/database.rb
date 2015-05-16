0ed1df00b3097461c985850e7a960583b77eac30
Andrew Gilbert <andrewg800@gmail.com>
2015-05-16 08:51:27 -0700

    Create a class to handle connections to the DB
    It really just delegates to Sequel; it allows us to do the connection
    somewhere standard, however.

---
+++ lib/database.rb
@@ -0,0 +1,44 @@
+require 'delegate'
+require 'sequel'
+
+##
+# This class provides a convenient way to get access to the database. It will
+# read the correct environment variables to find where it is supposed to be
+# connecting.
+class Database < Delegator
+
+   def initialize database
+      @db = Sequel.connect adapter: 'mysql2',
+         host: ENV["mysql_host"],
+         database: database,
+         password: ENV["mysql_password"],
+         user: ENV["mysql_user"]
+
+      super @db
+   end
+
+   def __getobj__
+      @db
+   end
+
+   def __setobj__ obj
+      @db = obj
+   end
+
+   ## Sources the given file into the DB
+   def source file
+      open(file) do |reader|
+         reader.map{|line| line.gsub /--.*$/, ''}.join.split(';').each do |sql|
+            if not sql.strip.empty?
+               begin
+               # Run the SQL on the DB
+               run sql
+               rescue => e
+                  p sql
+                  raise e
+               end
+            end
+         end
+      end
+   end
+end
