da051d3dafc80813518b124ff2607e624eabddad
Andrew Gilbert <andrewg800@gmail.com>
2015-05-26 21:55:51 -0700

    Move splitting of cleansed records out of Main

---
+++ lib/clean-splitter.rb
@@ -0,0 +1,100 @@
+module Splitter
+   class CleanSplitter
+      def initialize db
+         @db = db
+      end
+      def insert_phone record
+         if not record[:phone].nil?
+            data = record.filter [:phone]
+            id = @db.insert_phone data
+         else
+            id = nil
+         end
+         record[:phone_id] = id
+      end
+
+      def insert_address record
+         source = [
+            :mStreet,
+            :mUnit,
+            :mCity,
+            :mRegion,
+            :mPostCode,
+            :mCounty,
+            :mCountry]
+
+         source2 = [
+            :pStreet,
+            :pUnit,
+            :pCity,
+            :pRegion,
+            :pPostCode,
+            :pCounty,
+            :pCountry]
+
+         destination = [
+            :street,
+            :unit,
+            :city,
+            :region,
+            :postcode,
+            :county,
+            :country]
+
+         data = record.filter source, destination
+         data2 = record.filter source2, destination
+         record[:mAddress_id] = @db.insert_address data
+         record[:pAddress_id] = @db.insert_address data2
+      end
+
+      def insert_specialty record
+         source = [:primarySpecialty]
+         source2 = [:secondarySpecialty]
+         dest = [:code]
+         data = record.filter source, dest
+         data2 = record.filter source2, dest
+         record[:primarySpecialty_id] = @db.insert_specialty data
+         record[:secondarySpecialty_id] = @db.insert_specialty data2
+      end
+
+      def insert_cprovider record
+         source = [
+            :id,
+            :type,
+            :name
+         ]
+         data = record.filter source, {mAddress_id: :mailingAddress, pAddress_id: :practiceAddress, phone_id: :phone, primarySpecialty_id: :primarySpecialty, secondarySpecialty_id: :secondarySpecialty}
+         @db.insert_cprovider data
+      end
+
+      def insert_cprovidertype record
+         case record[:type]
+         when /individual/i
+            insert_cindividual record
+         when /organization/i
+            insert_corganization record
+         else
+            raise "Unexpected record type: #{record[:id]} has type '#{record[:type]}'"
+         end
+      end
+
+      def insert_cindividual record
+         source = [
+            :gender,
+            :dateOfBirth,
+            :isSoleProprietor,
+            :id
+         ]
+         data = record.filter source
+         record[:cIndividual_id] = @db.insert_cindividual data
+      end
+
+      def insert_corganization record
+         source = [
+            :id
+         ]
+         data = record.filter source
+         record[:cOrganization_id] = @db.insert_corganization data
+      end
+   end
+end
--- lib/main.rb
+++ lib/main.rb
@@ -3,6 +3,7 @@ require 'sequel'
 require 'psych'
 require_relative '../lib/database.rb'
 require 'hash'
+require 'clean-splitter'
 
 Sequel.extension :check_insert
 
@@ -11,6 +12,7 @@ class Main
    def initialize db, cleanser
       @db = db
       @cleanser = cleanser
+      @csplitter = Splitter::CleanSplitter.new @db
    end
 
    def run argv
@@ -49,18 +51,11 @@ class Main
       records.each do |record|
          cleansed = @cleanser.cleanse record
 
-         insert_phone record
-         insert_address record
-         insert_specialty record
-         insert_cprovider record
-         case record[:type]
-         when /individual/i
-            insert_cindividual record
-         when /organization/i
-            insert_corganization record
-         else
-            raise "Unexpected record type: #{record[:id]} has type '#{record[:type]}'"
-         end
+         @csplitter.insert_phone record
+         @csplitter.insert_address record
+         @csplitter.insert_specialty record
+         @csplitter.insert_cprovider record
+         @csplitter.insert_cprovidertype record
 
          count += 1
          if count % 100 == 0
@@ -224,87 +219,5 @@ class Main
 
       @db.insert_audit audit1
    end
-
-   def insert_phone record
-      if not record[:phone].nil?
-         data = record.filter [:phone]
-         id = @db.insert_phone data
-      else
-         id = nil
-      end
-      record[:phone_id] = id
-   end
-
-   def insert_address record
-      source = [
-         :mStreet,
-         :mUnit,
-         :mCity,
-         :mRegion,
-         :mPostCode,
-         :mCounty,
-         :mCountry]
-
-      source2 = [
-         :pStreet,
-         :pUnit,
-         :pCity,
-         :pRegion,
-         :pPostCode,
-         :pCounty,
-         :pCountry]
-
-      destination = [
-         :street,
-         :unit,
-         :city,
-         :region,
-         :postcode,
-         :county,
-         :country]
-
-      data = record.filter source, destination
-      data2 = record.filter source2, destination
-      record[:mAddress_id] = @db.insert_address data
-      record[:pAddress_id] = @db.insert_address data2
-   end
-
-   def insert_specialty record
-      source = [:primarySpecialty]
-      source2 = [:secondarySpecialty]
-      dest = [:code]
-      data = record.filter source, dest
-      data2 = record.filter source2, dest
-      record[:primarySpecialty_id] = @db.insert_specialty data
-      record[:secondarySpecialty_id] = @db.insert_specialty data2
-   end
-
-   def insert_cprovider record
-      source = [
-         :id,
-         :type,
-         :name
-      ]
-      data = record.filter source, {mAddress_id: :mailingAddress, pAddress_id: :practiceAddress, phone_id: :phone, primarySpecialty_id: :primarySpecialty, secondarySpecialty_id: :secondarySpecialty}
-      @db.insert_cprovider data
-   end
-
-   def insert_cindividual record
-      source = [
-         :gender,
-         :dateOfBirth,
-         :isSoleProprietor,
-         :id
-      ]
-      data = record.filter source
-      record[:cIndividual_id] = @db.insert_cindividual data
-   end
-
-   def insert_corganization record
-      source = [
-         :id
-      ]
-      data = record.filter source
-      record[:cOrganization_id] = @db.insert_corganization data
    end
 end
