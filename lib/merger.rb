516fae283133178343484f6fd6cbf3ad4721a0bd
Andrew Gilbert <andrewg800@gmail.com>
2015-06-06 02:30:54 -0700

    Remove extra definition of Array
    This shouldn't have been around.

--- lib/merger.rb
+++ lib/merger.rb
@@ -125,19 +125,3 @@ class Merger
       count
    end
 end
-
-class Array
-   def split n
-      max = length
-
-      i = 0
-      while i+n < max
-         yield self[i..i+n]
-         i += n
-      end
-
-      if i < max
-         yield self[i..-1]
-      end
-   end
-end
