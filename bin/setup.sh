6425759ec0aa1d89b4b4a1335998ffbfd2f156c7
Andrew Gilbert <andrewg800@gmail.com>
2015-06-06 20:05:58 -0700

    Remove runtime dependency on Log4J

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,4 @@
-log4japi="$(readlink -e /usr/share/java/log4j-api.jar)"
-log4jcore="$(readlink -e /usr/share/java/log4j-core.jar)"
-classpath="$CLASSPATH:$log4japi:$log4jcore"
+classpath="$CLASSPATH"
 export CLASSPATH="$classpath"
 if [ -n "$1" ]
 then
