ebdf07b42b7872966de4499f9fe5b24723191b79
Andrew Gilbert <andrewg800@gmail.com>
2015-06-08 06:06:29 -0700

    Add DB-create to create the DBs needed

---
+++ DB-create.sql
@@ -0,0 +1,5 @@
+create database automatons;
+create database automatons_test;
+create user 'automatons'@'localhost' identified by 'z8eEnVt*';
+grant all on automatons.* to 'automatons'@'localhost';
+grant all on automatons_test.* to 'automatons'@'localhost';
